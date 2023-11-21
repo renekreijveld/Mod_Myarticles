e804212ff3c29ba8dee66ecbacb3cd9b0e678961
Rene Kreijveld <email@renekreijveld.nl>
2023-11-21 21:13:26 +0100

    Added link to article and added edit link

--- mod_myarticles/tmpl/default.php
+++ mod_myarticles/tmpl/default.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
 <ul class="mod-myarticles list-group">
     <?php foreach ( $list as $article ): ?>
         <li class="list-group-item">
-            <?php echo $article->title; ?>
+            <a href="/index.php?option=com_content&view=article&id=<?php echo $article->id; ?>"><?php echo $article->title; ?></a>&nbsp;<a href="/index.php?option=com_content&task=article.edit&a_id=<?php echo $article->id; ?>">Edit</a>
         </li>
     <?php endforeach; ?>
 </ul>
